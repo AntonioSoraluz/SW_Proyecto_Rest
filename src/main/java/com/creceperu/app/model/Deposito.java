123c8b56580a3b7c22b0421ba21fa88c808f2d57
CastroPaulo10 <i201812338@cibertec.edu.pe>
2023-04-27 18:58:50 -0500

    DEPOSITO COMMIT

---
+++ src/main/java/com/creceperu/app/model/Deposito.java
@@ -0,0 +1,57 @@
+package com.creceperu.app.model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Deposito {
+
+	   @Id
+	    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	    private Long id;
+	    
+	    private String numeroCuenta;
+	    
+	    private double monto;
+	    
+	    private Date fecha;
+
+		public Long getId() {
+			return id;
+		}
+
+		public void setId(Long id) {
+			this.id = id;
+		}
+
+		public String getNumeroCuenta() {
+			return numeroCuenta;
+		}
+
+		public void setNumeroCuenta(String numeroCuenta) {
+			this.numeroCuenta = numeroCuenta;
+		}
+
+		public double getMonto() {
+			return monto;
+		}
+
+		public void setMonto(double monto) {
+			this.monto = monto;
+		}
+
+		public Date getFecha() {
+			return fecha;
+		}
+
+		public void setFecha(Date fecha) {
+			this.fecha = fecha;
+		}
+	
+	    
+	    
+}
---
+++ src/main/java/com/creceperu/app/repository/DepositoRepository.java
@@ -0,0 +1,9 @@
+package com.creceperu.app.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.creceperu.app.model.Deposito;
+
+public interface DepositoRepository extends JpaRepository<Deposito, Long>{
+
+}
