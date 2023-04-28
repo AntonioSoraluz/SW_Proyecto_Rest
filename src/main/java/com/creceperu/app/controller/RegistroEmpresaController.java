4d1f5c8a912bae90f8be0456e1f448b67015a2e5
ANDERSON <i202020454@cibertec.edu.pe>
2023-04-27 20:26:39 -0500

    test

--- src/main/java/com/creceperu/app/controller/RegistroEmpresaController.java
+++ src/main/java/com/creceperu/app/controller/RegistroEmpresaController.java
@@ -1,30 +1,30 @@
-//package com.creceperu.app.controller;
-//
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.ModelAttribute;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import com.creceperu.app.controller.dto.EmpresaRegistroDTO;
-//import com.creceperu.app.service.EmpresaService;
-//
-//@Controller
-//@RequestMapping("/registroEmpresa")
-//public class RegistroEmpresaController {
-//
-//	private EmpresaService empresaService;
-//
-//	public RegistroEmpresaController(EmpresaService empresaService) {
-//		super();
-//		this.empresaService = empresaService;
-//	}
-//
-//	@ModelAttribute("empresa")
-//	public EmpresaRegistroDTO retornarNuevaEmpresaRegistroDTO() {
-//		return new EmpresaRegistroDTO();
-//	}
-//
-//	@GetMapping
-//	public String mostrarFormularioDeRegistro() {
-//		return "registroEmpresa";
-//	}
-//}
+package com.creceperu.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import com.creceperu.app.controller.dto.EmpresaRegistroDTO;
+import com.creceperu.app.service.EmpresaService;
+
+@Controller
+@RequestMapping("/registroEmpresa")
+public class RegistroEmpresaController {
+
+	private EmpresaService empresaService;
+
+	public RegistroEmpresaController(EmpresaService empresaService) {
+		super();
+		this.empresaService = empresaService;
+	}
+
+	@ModelAttribute("empresa")
+	public EmpresaRegistroDTO retornarNuevaEmpresaRegistroDTO() {
+		return new EmpresaRegistroDTO();
+	}
+
+	@GetMapping
+	public String mostrarFormularioDeRegistro() {
+		return "registroEmpresa";
+	}
+}
