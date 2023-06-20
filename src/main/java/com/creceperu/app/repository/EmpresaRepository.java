15c239cf0337ecdf52ab763b6c4bed26668ca195
Antonio Soraluz <soraluztorresjoseantonio@gmail.com>
2023-06-20 15:39:29 -0500

    Se realizó pequeñas correcciones de diseño

--- src/main/java/com/creceperu/app/repository/EmpresaRepository.java
+++ src/main/java/com/creceperu/app/repository/EmpresaRepository.java
@@ -1,10 +1,6 @@
 package com.creceperu.app.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.creceperu.app.model.Empresa;
